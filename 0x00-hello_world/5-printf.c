9637db428f1dc9b1a565d784b993dd53e062eb84
Martins Okoro <Wfkvngmartinez1991@outlook.com>
2023-09-10 15:53:39 -0700

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
-* main - Entry point of the program
-*
-* Description: Prints a messagewithpropergrammar,
-* buttheoutcomeisapieceofart,
-* followedbyanewline.
-*
-* Return: Always0(Success)
+* main - A program that prints a line using the printf function
+* Return: 0 (Success)
 */
+
 int main(void)
 {
-printf("withpropergrammar,buttheoutcomeisapieceofart,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
