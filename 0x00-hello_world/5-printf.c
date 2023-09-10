844410f67695f07495472359afaa230284d1bdb2
Martins Okoro <Wfkvngmartinez1991@outlook.com>
2023-09-10 15:43:22 -0700

    5-print.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,16 @@
-
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: Prints a message with proper grammar,
- *              but the outcome is a piece of art,
- *              followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+*
+* Description: Prints a messagewithpropergrammar,
+* buttheoutcomeisapieceofart,
+* followedbyanewline.
+*
+* Return: Always0(Success)
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
-
+printf("withpropergrammar,buttheoutcomeisapieceofart,\n");
+return (0);
+}
