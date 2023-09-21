6b4e041b7eb008e50441355e6fe519989070d71c
Martins Okoro <Wfkvngmartinez1991@outlook.com>
2023-09-20 18:17:12 -0700

    0-strcat.c  task commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,24 @@
 #include "main.h"
 /**
  * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- * Return: void
+ * @dest: the first char
+ * @src: the second char
+ * Return: orgdest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	char *orgdst = dest;
 
-	i = 0;
-	while (dest[i] != '\0')
+	while (*dest != '\0')
 	{
-		i++;
+		dest++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	while (*src != '\0')
 	{
-		dest[i]	= src[j];
-		i++;
-		j++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	dest[i]	= '\0';
-	return (dest);
+	*dest = '\0';
+	return (orgdest);
 }
